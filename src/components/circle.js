b945185f06da75ea5d21e400e27dd808e23855b2
prerna ghante <45898992+peri6@users.noreply.github.com>
2022-05-26 10:54:52 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,14 @@
 // import using require
 
 // declare class
-
+class Circle extends drawShape{
+  calculateArea(radius){
+    console.log("Area is"+3.14*radius*radius);
+  }
+  
+  color(){
+    console.log("Red");
+  }
+}
 
 // export class using module.exports
