740275abaa98db0907978148885b043a29c3ae6b
prerna ghante <45898992+peri6@users.noreply.github.com>
2022-05-26 10:58:54 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
-
+class Shape{
+  drawShape(radius,color){
+  this.radius = radius;
+  this.color = color;
+  }
+  calculateArea(){
+    return '${this.radius}';
+  }
+  
+  color(){
+    return'Color:${this.color}';
+  }
+  
+}
+module.exports = user;
 // export class using module.exports
